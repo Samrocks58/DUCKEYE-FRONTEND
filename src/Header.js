c4dcab508fd938fb85c71dcdb75a45cb838bae63
Shiven Pandya <shiven.pandya@peer-consulting.com>
2025-03-30 07:47:00 -0400

    Centered DUCKEYE logo on all pages

--- src/Header.js
+++ src/Header.js
@@ -5,37 +5,37 @@ const Header = () => {
   const navigate = useNavigate();
   
   return (
-    <div style={{paddingBottom: "95px"}}>
-    <header style={styles.header}>
-      {/* Logo on the left */}
-      <img
-        src="transparent duck LOGO.png"
-        alt="DUCKEYE LOGO"
-        style={styles.logo}
-        onClick={() => navigate("/")}
-      />
-      
-      {/* Centered Text */}
-      
-      <div style={styles.title}>DUCKEYE</div>
-    </header>
+    <div style={{ paddingBottom: "95px" }}>
+      <header style={styles.header}>
+        {/* Logo on the left */}
+        <img
+          src="transparent duck LOGO.png"
+          alt="DUCKEYE LOGO"
+          style={styles.logo}
+          onClick={() => navigate("/")}
+        />
+        {/* Centered Text */}
+        <div style={styles.title}>DUCKEYE</div>
+      </header>
     </div>
   );
 };
 
-// CSS-in-JS styles
 const styles = {
   header: {
-    position: "absolute",
+    position: "relative", // Changed from absolute to relative
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
     backgroundColor: "#ADD8E6",
-    paddingTop: "40px",
-    paddingBottom: "40px",
-    width: "100%"
+    padding: "40px 0",
+    width: "100%",
   },
   logo: {
     position: "absolute",
-    left: "10px", // Keep the logo on the left
-    top: "5px",
+    left: "10px",
+    top: "50%",
+    transform: "translateY(-50%)",
     width: "128px",
     height: "92px",
     cursor: "pointer",
@@ -43,7 +43,7 @@ const styles = {
   title: {
     fontSize: "24px",
     fontWeight: "bold",
-    paddingLeft: "550px",
+    textAlign: "center",
   },
 };
 
